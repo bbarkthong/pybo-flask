4bc3da13b10d458e3a2e4e98b8d48658401b48c0
bbarkthong <bbarkthong@gmail.com>
2024-02-28 17:59:58 +0900

    #4 템플릿 필터 만들기

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -16,10 +16,14 @@ def create_app():
     migrate.init_app(app, db)
     from . import models
     
-    # Blue Print
+    # 블루프린트
     from .views import main_views, question_views, answer_views
     app.register_blueprint(main_views.bp)
     app.register_blueprint(question_views.bp)
     app.register_blueprint(answer_views.bp)
+    
+    # 필터
+    from .filter import format_datetime
+    app.jinja_env.filters['datetime'] = format_datetime
     
     return app
